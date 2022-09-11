fce7ea4d98a08f5fb6bfb924e197cab0364ddcd6
Replit user <>
2022-09-11 21:30:30 +0200

    first commit

--- server.js
+++ server.js
@@ -7,18 +7,30 @@ fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
+const bcrypt = require('bcrypt');
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash)=>{
+  // console.log('myPlaintextPassword', myPlaintextPassword);
+  // console.log('saltRounds', saltRounds);
+  console.log(hash)
 
+   //check if a new input is the same data as the hash
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) =>{
+    console.log(res)
+  });
+
+});
 
 
 //END_ASYNC
 
 //START_SYNC
-
-
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds)
+console.log(hash)
+var result = bcrypt.compareSync(myPlaintextPassword, hash)
+console.log(result)
 //END_SYNC
 
 
